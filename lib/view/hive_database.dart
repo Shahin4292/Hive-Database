2f647440e5db6fb53ba80806da488fe252579457
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-11-11 15:12:37 +0600

    add or update function done

--- lib/view/hive_database.dart
+++ lib/view/hive_database.dart
@@ -1,13 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 
-class HiveDatabase extends StatelessWidget {
+class HiveDatabase extends StatefulWidget {
   const HiveDatabase({super.key});
+
+  @override
+  State<HiveDatabase> createState() => _HiveDatabaseState();
+}
+
+class _HiveDatabaseState extends State<HiveDatabase> {
+  var dataStorageBox = Hive.box("MyBox");
+
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _ageController = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _ageController.dispose();
+    super.dispose();
+  }
+
+  void addOrUpdate({String? key}) {
+    if (key != null) {
+      final person = dataStorageBox.get(key);
+      if (person != null) {
+        _nameController.text = person["name"] ?? "";
+        _ageController.text = person["age"]?.toString() ?? "";
+      }
+    } else {
+      _nameController.clear();
+      _ageController.clear();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
+        centerTitle: true,
         backgroundColor: Colors.blue[50],
         title: const Text("Hive Database"),
       ),
